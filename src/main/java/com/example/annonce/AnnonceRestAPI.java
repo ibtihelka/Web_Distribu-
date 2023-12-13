736db515f70ceff12350956179d6656bd8fc51d7
chadhahannachi <chadha.hannachi@esprit.tn>
2023-12-13 04:02:39 +0100

    modif

--- src/main/java/com/example/annonce/AnnonceRestAPI.java
+++ src/main/java/com/example/annonce/AnnonceRestAPI.java
@@ -6,16 +6,23 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/annonce")
+
+
 public class AnnonceRestAPI {
     @Autowired
     private AnnonceService annonceService;
-    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Annonce> createAnnonce(@RequestBody Annonce annonce) {
         return new ResponseEntity<>(annonceService.addAnnonce(annonce), HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Annonce> updateAnnonce(@PathVariable(value = "id") int id,
@@ -23,10 +30,23 @@ public class AnnonceRestAPI {
         return new ResponseEntity<>(annonceService.updateAnnonce(id, annonce),
                 HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> deleteAnnonce(@PathVariable(value = "id") int id){
         return new ResponseEntity<>(annonceService.deleteAnnonce(id), HttpStatus.OK);
     }
-    
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping
+    public ResponseEntity<List<Annonce>> getAllAnnonces() {
+        List<Annonce> annonces = annonceService.getAllAnnonces(); // Utilisation de l'instance annonceService
+        if (annonces != null) {
+            return new ResponseEntity<>(annonces, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/main/java/com/example/annonce/AnnonceService.java
+++ src/main/java/com/example/annonce/AnnonceService.java
@@ -3,6 +3,8 @@ package com.example.annonce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AnnonceService {
     @Autowired
@@ -11,6 +13,7 @@ public class AnnonceService {
     public Annonce addAnnonce(Annonce annonce) {
         return annonceRepository.save(annonce);
     }
+
 
     public Annonce updateAnnonce(int id, Annonce newAnnonce) {
         if (annonceRepository.findById(id).isPresent()) {
@@ -22,6 +25,9 @@ public class AnnonceService {
         } else
             return null;
     }
+
+
+
     public String deleteAnnonce(int id) {
         if (annonceRepository.findById(id).isPresent()) {
             annonceRepository.deleteById(id);
@@ -29,4 +35,44 @@ public class AnnonceService {
         } else
             return "annonce non supprimé";
     }
+
+
+    public List<Annonce> getAllAnnonces() {
+        return annonceRepository.findAll();
+    }
+
+    /*public Visite addvisite(Visite s){
+        return visiterepo.save(s);
+    }
+
+    public Visite updateVisite(Long id, Visite newVisite) {
+        if (visiterepo.findById(id).isPresent()) {
+            Visite existingVisit = visiterepo.findById(id).get();
+            existingVisit.setNom(newVisite.getNom());
+            existingVisit.setPrenom(newVisite.getPrenom());
+            existingVisit.setEmail(newVisite.getEmail());
+            return visiterepo.save(existingVisit);
+        } else
+            return null;
+    }
+
+
+    public String deleteVisit(Long id) {
+        if (visiterepo.findById(id).isPresent()) {
+            visiterepo.deleteById(id);
+            return "candidat supprimé";
+        } else
+            return "candidat non supprimé";
+    }
+
+
+    public List<Annonce> getAllAnnonces() {
+        return annoncerepository.findAll();
+    }
+
+    public Optional<Visite> getVisiteById(Long id) {
+        return visiterepo.findById(id);
+    }
+
+}*/
 }
